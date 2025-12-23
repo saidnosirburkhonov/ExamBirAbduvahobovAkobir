1e5e8df1bc6ec8dc840b0ffb3fc70929e6404c56
Saidnosir Burkhonov <saidnosirburxonov.com>
2025-12-23 15:45:55 +0500

    done

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Exam1;
+
+internal class Program
+{
+    static List<int> lst = new List<int>() { 22, 1, 2, 1, 2, 12 };
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Task6(lst));
+    }
+    static int Task1(List<int> nums)
+    {
+        var count = 0;
+        foreach (var num in nums)
+        {
+            if (num % 2 == 0)
+            {
+                ++count;
+            }
+        }
+        return count;
+    }
+    static bool Task2(List<string> text)
+    {
+        return text.All(x => x.Length > 5);
+    }
+    static string Task3(string text)
+    {
+        return text.Substring(0, 3);
+    }
+    static bool Task4(string text)
+    {
+        if (text.StartsWith("PDP"))
+        {
+            return true;
+        }
+        return false;
+    }
+    static bool Task5(string text)
+    {
+        if (text.Contains("G13"))
+        {
+            return true;
+        }
+        return false;
+    }
+    static int Task6(List<int> nums)
+    {
+        var count = nums[0] + nums[1];
+        return count;
+    }
+}
